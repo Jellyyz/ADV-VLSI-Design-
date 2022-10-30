6c4238beec1f5af58b50196fd16359522cc9b248
siva3 <siva3@ece-498hk-01.ece.illinois.edu>
2022-10-30 16:25:43 -0500

    Instantiated an Ibex core with magic memory

--- hvl/magic_mem_sim/top.sv
+++ hvl/magic_mem_sim/top.sv
@@ -131,47 +131,49 @@ ibex_top #(
     .rst_ni                 (itf.rst),
     .test_en_i              (1'b1), //this makes sure that the chip doesnt do any clock gating
     .scan_rst_ni            (1'b1), // document told to tie this off to 1 as we don't have dft
-    .ram_cfg_i              (),
+    .ram_cfg_i              ('0), //all the DV files seem to be setting this to 0
 
     // Configuration
-    .hart_id_i              (),
-    .boot_addr_i            (),
+    .hart_id_i              ('0), //setting to 0 as that's what everyone else seems to be doing
+    .boot_addr_i            ('0), // this should make it boot at address 0x80
 
     // Instruction memory interface
-    .instr_req_o            (),
-    .instr_gnt_i            (),
-    .instr_rvalid_i         (),
-    .instr_addr_o           (),
-    .instr_rdata_i          (),
-    .instr_rdata_intg_i     (),
-    .instr_err_i            (),
+    .instr_req_o            (itf.inst_read),
+    .instr_gnt_i            (itf.inst_resp),
+    .instr_rvalid_i         (itf.inst_resp),
+    .instr_addr_o           (itf.inst_addr),
+    .instr_rdata_i          (itf.inst_rdata),
+    .instr_rdata_intg_i     ('0), //that's what a bunch of examples did, so doesn't seem too unreasonable
+    .instr_err_i            ('b0),
 
     // Data memory interface
-    .data_req_o             (),
-    .data_gnt_i             (),
-    .data_rvalid_i          (),
-    .data_we_o              (),
-    .data_be_o              (),
-    .data_addr_o            (),
-    .data_wdata_o           (),
-    .data_wdata_intg_o      (),
-    .data_rdata_i           (),
-    .data_rdata_intg_i      (),
-    .data_err_i             (),
+    .data_req_o             (itf.data_req),
+    .data_gnt_i             (itf.data_resp),
+    .data_rvalid_i          (itf.data_resp),
+    .data_we_o              (itf.data_wen),
+    .data_be_o              (itf.data_mbe),
+    .data_addr_o            (itf.data_addr),
+    .data_wdata_o           (itf.data_wdata),
+    .data_wdata_intg_o      ('0), //the FPGA example sets this to '0, so so am I
+    .data_rdata_i           (itf.data_rdata),
+    .data_rdata_intg_i      ('0), 
+    .data_err_i             ('b0),
 
     // Interrupt inputs
-    .irq_software_i         (),
-    .irq_timer_i            (),
-    .irq_external_i         (),
-    .irq_fast_i             (),
-    .irq_nm_i               (),
+    //All of these were copied from the artya example
+    .irq_software_i         (1'b0),
+    .irq_timer_i            (1'b0),
+    .irq_external_i         (1'b0),
+    .irq_fast_i             (15'b0),
+    .irq_nm_i               (1'b0),
 
     // Debug interface
-    .debug_req_i            (),
+    //again values copied from the FPGA example
+    .debug_req_i            ('b0),
     .crash_dump_o           (),
 
     // Special control signals
-    .fetch_enable_i         (),
+    .fetch_enable_i         ('b1),
     .alert_minor_o          (),
     .alert_major_internal_o (),
     .alert_major_bus_o      (),
